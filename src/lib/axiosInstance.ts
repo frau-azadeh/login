fd17a9779adc5b5154c73a44913769846a44f5c5
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-20 08:13:34 -0800

    add api

---
+++ src/lib/axiosInstance.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const authApi = axios.create({
+    baseURL:"https://67b59f1807ba6e59083dafc9.mockapi.io/",
+    headers:{
+        "Content-Type":"application/json",
+    }
+})
