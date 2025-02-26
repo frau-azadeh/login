3f38374aa49e50f0c37ea9d8809e9a56d3c3128f
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-26 00:36:20 -0800

    update

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+"use client"
+import React from "react"
+import DatePicker from "react-multi-date-picker"
+import persian from "react-date-object/calendars/persian"
+import persian_fa from "react-date-object/locales/persian_fa"
+import { Calendar } from "react-multi-date-picker"
 
-export const Calendar = () => {
+export default function Example() {
   return (
-    <div>page</div>
+    <div style={{ direction: "rtl" }}>
+      <DatePicker
+        calendar={persian}
+        locale={persian_fa}
+        calendarPosition="bottom-right"
+      />
+    <div style={{direction:"ltr"}}>
+      <Calendar
+              calendar={persian}
+              locale={persian_fa}
+            />
+    </div>
+    </div>
   )
 }
