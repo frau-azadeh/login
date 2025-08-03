52f2624a3db780bf5a0f42b35aab95cdc3dcbd7f
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-03 10:17:25 +0330

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,7 @@
-"use client";
-
-import { authApi } from "@/lib/axiosInstance";
-import { useEffect, useState } from "react";
-import { User } from "@/types/user";
-
-export default function HomePage () {
-  const [userLogin, setUserLogin] = useState<User[]>([]);
-
-  useEffect(()=>{
-    const fetchData = async () =>{
-      try{
-        const response = await authApi.get<User[]>('/login');
-        setUserLogin(response.data);
-
-      }catch(error){
-        console.error('fail', error);
-      }
-    }
-    fetchData();
-  },[]);
-
+export default function Home(){
   return(
     <div>
-      <ul>
-        {userLogin.map((user)=>(
-          <li key={user.id}>
-            <p>{user.fullName}</p>
-            <p>{user.role}</p>
-          </li>
-        ))}
-      </ul>
+      <h1>پنل لاگین</h1>
     </div>
   )
 }
